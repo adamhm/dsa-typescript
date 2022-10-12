6c4a8e40d4378927243b1ff650ebf873f8fbff57
Adam Hamar <adam.hamar@gmail.com>
2022-10-12 10:22:32 +0200

    feat: add recursive binary search implementation

---
+++ src/search/binarySearchRecursive.ts
@@ -0,0 +1,23 @@
+function binarySearchRecursive(array: number[], target: number): number {
+    return binaryRecursive(array, target, 0, array.length);
+}
+
+function binaryRecursive(array: number[], target: number, start: number, end: number): number {
+    if (start > end) {
+        return -1;
+    }
+
+    const mid = Math.floor(start + (end - start) / 2);
+
+    if (array[mid] === target) {
+        return mid;
+    }
+
+    if (array[mid] > target) {
+        return binaryRecursive(array, target, start, mid - 1);
+    }
+
+    return binaryRecursive(array, target, mid + 1, end);
+}
+
+export default binarySearchRecursive;
